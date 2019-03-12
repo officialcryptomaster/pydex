02abd7c4adf31116b4c6a45c4f92b493b1d05351
cryptomaster <officialcryptomaster@gmail.com>
2019-03-12 01:02:24 -0700

    Add some more datetime functions and type annotations to miscutils

--- src/utils/miscutils.py
+++ src/utils/miscutils.py
@@ -8,33 +8,43 @@ from datetime import datetime
 from decimal import Decimal
 
 
-def now_epoch_secs():
+def now_epoch_secs() -> int:
     """integer seconds epoch of current time"""
     return int(time.time())
 
 
-def now_epoch_msecs():
+def now_epoch_msecs() -> int:
     """integer milliseconds epoch of current time"""
     return int(time.time() * 1e3)
 
 
-def now_epoch_usecs():
+def now_epoch_usecs() -> int:
     """integer micro-seconds epoch of current time"""
     return int(time.time() * 1e6)
 
 
-def epoch_secs_to_local_time_str(epoch_secs):
+def epoch_secs_to_local_time_str(epoch_secs) -> str:
     """Get a string of local time representation from integer epoch in seconds"""
     return datetime.fromtimestamp(epoch_secs).strftime("%Y-%m-%d %H:%M:%S")
 
 
-def epoch_msecs_to_local_time_str(epoch_msecs):
+def epoch_msecs_to_local_time_str(epoch_msecs) -> str:
     """Get a string of local time representation from integer epoch in milliseconds"""
     return datetime.fromtimestamp(epoch_msecs/1000).strftime("%Y-%m-%d %H:%M:%S")
 
 
+def epoch_secs_to_local_datetime(epoch_secs) -> datetime:
+    """Get a string of local time representation from integer epoch in seconds"""
+    return datetime.fromtimestamp(epoch_secs)
+
+
+def epoch_msecs_to_local_datetime(epoch_msecs) -> datetime:
+    """Get a string of local time representation from integer epoch in milliseconds"""
+    return datetime.fromtimestamp(epoch_msecs/1000)
+
+
 def try_(func, *args, **kwargs):
-    """Try to call a function and return _default if it fails
+    """Try to call a function and return `_default` if it fails
     Note: be careful that in order to have a fallback, you can supply
     the keyword argument `_default`. If you supply anything other
     than a keyword arg, it will result in it being passed to the wrapped
@@ -66,13 +76,3 @@ def paginate(arr, page=1, per_page=20):
     """
     page_idx = page - 1
     return arr[page_idx: page_idx+per_page]
-
-
-def normalize_query_param(query_param):
-    """Normalize query parameter to lower case"""
-    return query_param.lower() if query_param else None
-
-
-def to_api_order(signed_order_json):
-    """Given a signed order json, make compatible with 0x API Order Schema"""
-    return {"metaData": {}, "order": signed_order_json}
